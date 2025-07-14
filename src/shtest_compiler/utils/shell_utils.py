e869560015a8ba2a847f684b46eef8332e1f1b3e
Rayane <rayane.zekik@epita.fr>
2025-07-14 20:51:10 +0200

    fix: ressource paths

--- src/shtest_compiler/utils/shell_utils.py
+++ src/shtest_compiler/utils/shell_utils.py
@@ -4,8 +4,7 @@ import os
 def resource_path(relative_path):
     """Get absolute path to resource, works for dev and for PyInstaller."""
     if hasattr(sys, '_MEIPASS'):
-        list_meipass()
-        return os.path.join(sys._MEIPASS, 'shtest_compiler', relative_path)
+        return os.path.join(sys._MEIPASS, relative_path)
     # Join from the src root, not from the utils directory
     return os.path.join(os.path.dirname(os.path.dirname(__file__)), relative_path)
 
