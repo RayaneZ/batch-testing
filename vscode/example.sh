71fa96554913da1ea6b08fd4645ffdd231efa83f
Rayane <rayane.zekik@epita.fr>
2025-07-14 19:07:55 +0200

    feat: ci orchestrator

--- vscode/example.sh
+++ vscode/example.sh
@@ -1,2 +1,2 @@
-echo "[ERROR] ValidationParseError: Validation context errors for 'le fichier est présent': Handler file_present requires 'file' variable"
+echo "[ERROR] ValidationParseError: Validation context errors for 'le dossier est créé': Handler dir_exists requires 'dir' variable"
 exit 1
